5a6604f709f26c3ddb7b8d2c71381eb38cdc4083
Rocky <mdrockibul@gmail.com>
2017-09-27 15:18:17 +0600

    simple launcher

---
+++ app/src/main/java/com/azmar/mylauncher/Item.java
@@ -0,0 +1,13 @@
+package com.azmar.mylauncher;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Rocky on 9/7/2017.
+ */
+
+public class Item {
+    CharSequence label; //package name
+    CharSequence name; //app name
+    Drawable icon; //app icon
+}
